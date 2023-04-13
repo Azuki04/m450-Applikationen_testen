98fd8f7d826eaea4ddf5e95b50817852dfcb938d
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-04-13 11:09:29 +0200

    Update

--- Back-End/src/test/java/ch/web/web_shop/ProductControllerTest.java
+++ Back-End/src/test/java/ch/web/web_shop/ProductControllerTest.java
@@ -2,14 +2,22 @@
 package ch.web.web_shop;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
+import ch.web.web_shop.controller.NewslatterController;
+import ch.web.web_shop.model.Newslatter;
+import ch.web.web_shop.model.Product;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,6 +28,9 @@ import ch.web.web_shop.repository.CategoryRepository;
 import ch.web.web_shop.repository.NewslatterRepository;
 import ch.web.web_shop.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 @AutoConfigureMockMvc
@@ -29,12 +40,15 @@ public class ProductControllerTest {
 	@MockBean
 	private NewslatterRepository newslatterRepository;
 	@MockBean
-	private ProductRepository prpductRepository;
+	private ProductRepository productRepository;
 	@MockBean
 	private CategoryRepository categoryRepository;
 
 	@Autowired
 	ProductController productController;
+
+	@Autowired
+	NewslatterController newslatterController;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -71,13 +85,14 @@ public class ProductControllerTest {
 	@Test
 	public void testDeleteAllProducts() {
 		productRepository.deleteAll();
+
 		assertEquals(0, productRepository.count());
 	}
 
 	@Test
 	public void testDeleteAllProductsException() {
 		doThrow(new RuntimeException()).when(productRepository).deleteAll();
-		ResponseEntity<HttpStatus> response = controller.deleteAllProducts();
+		ResponseEntity<HttpStatus> response = productController.deleteAllProducts();
 		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
 	}
 
@@ -86,9 +101,9 @@ public class ProductControllerTest {
 	@Test
 	public void testCreateNewslatter() {
 		Newslatter newslatter = new Newslatter();
-		newslatter.setEmail("test@test.com");
+		newslatter.setEmailId("test@test.com");
 		when(newslatterRepository.save(newslatter)).thenReturn(newslatter);
-		assertEquals(newslatter, controller.createNewslatter(newslatter));
+		assertEquals(newslatter, newslatterController.createNewslatter(newslatter));
 	}
 
 
@@ -103,7 +118,7 @@ public class ProductControllerTest {
 		product.setPublished(true);
 		products.add(product);
 		when(productRepository.findByPublished(true)).thenReturn(products);
-		ResponseEntity<List<Product>> responseEntity = controller.findByPublished();
+		ResponseEntity<List<Product>> responseEntity = productController.findByPublished();
 		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
 		assertEquals(products, responseEntity.getBody());
 	}

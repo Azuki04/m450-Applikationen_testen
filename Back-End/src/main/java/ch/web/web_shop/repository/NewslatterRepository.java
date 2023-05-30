3672a6c41c21852abe70b9f2310286c71d65bb6a
Azuki04 <syviet@outlook.com>
2023-05-30 12:55:58 +0200

    Verbesserte App

--- Back-End/src/main/java/ch/web/web_shop/repository/NewslatterRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.web.web_shop.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ch.web.web_shop.model.Newslatter;
-
-public interface NewslatterRepository extends CrudRepository<Newslatter, Long> {
-
-}

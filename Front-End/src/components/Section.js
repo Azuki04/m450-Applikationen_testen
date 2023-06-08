6760f732eee7a54d12a5303e0abe0f10d68958b4
syviet <syviet@outlook.com>
2023-06-08 11:03:13 +0200

    implemment Login and Sign-In in Section.js

--- Front-End/src/components/Section.js
+++ Front-End/src/components/Section.js
@@ -11,26 +11,31 @@ import Cart from "./Page/Cart";
 import Home from "./Page/Home";
 import Payment from "./Page/Payment";
 import UserProfil from "./Page/UserProfil";
+import Login from "./Page/Login";
+import SignIn from "./Page/SignIn";
+
 
 class Section extends React.Component {
   render() {
     return (
-      <section>
-        <Routes>
-          <Route path="/">
-            <Route path="" index element={<Home />} />
-            <Route path="products" element={<Products />} />
-            <Route path="editProduct/:id" element={<EditProduct />} />
-            <Route path="products/detail/:id" element={<ProductDetails />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="payment" element={<Payment />} />
-            <Route path="add" element={<AddProduct />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="user/:id" element={<UserProfil />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </section>
+        <section>
+          <Routes>
+            <Route path="/" >
+              <Route path="" index element={<Login />} />
+              <Route path="sign-In" element={<SignIn/>} />
+              <Route path="home" element={<Home />} />
+              <Route path="products" element={<Products />} />
+              <Route path="editProduct/:id" element={<EditProduct />} />
+              <Route path="products/detail/:id" element={<ProductDetails />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="payment" element={<Payment />} />
+              <Route path="add" element={<AddProduct />} />
+              <Route path="about" element={<AboutUs />} />
+              <Route path="user/:id" element={<UserProfil />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </section>
     );
   }
 }

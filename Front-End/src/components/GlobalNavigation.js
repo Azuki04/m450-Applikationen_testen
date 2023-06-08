f5f618d07e04870eecc3d8fb12e0c707e5d1c90b
syviet <syviet@outlook.com>
2023-06-08 10:52:08 +0200

    GlobalNavigation.js Icon created for UserProfil.js

--- Front-End/src/components/GlobalNavigation.js
+++ Front-End/src/components/GlobalNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import Menu from "./svg/bars-solid.svg";
 import Close from "./svg/times-solid.svg";
 import CartIcon from "./svg/shopping-cart-solid.svg";
+import User from "./svg/user.svg";
 import { Link } from "react-router-dom";
 import "./css/GlobalNavigation.css";
 
@@ -17,44 +18,49 @@ class GlobalNavigation extends React.Component {
   render() {
     const { toggle } = this.state;
     return (
-      <header>
-        <div className="menu" onClick={this.menuToggle}>
-          <img src={Menu} alt="" width="20" />
-        </div>
-        <div className="logo">
-          <h1>
-            <Link to="/">Shop.ch</Link>
-          </h1>
-        </div>
-        <nav>
-          <ul className={toggle ? "toggle" : ""}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/products">my_Products</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/add">Create</Link>
-            </li>
-            <li className="close" onClick={this.menuToggle}>
-              <img src={Close} alt="" width="20" />
-            </li>
-          </ul>
-          <div className="nav-cart">
-            <span>0</span>
-            <Link to="/cart">
-              <img src={CartIcon} alt="" width="20" />
-            </Link>
+        <header>
+          <div className="menu" onClick={this.menuToggle}>
+            <img src={Menu} alt="" width="20" />
           </div>
-        </nav>
-      </header>
+          <div className="logo">
+            <h1>
+              <Link to="/">Shop.ch</Link>
+            </h1>
+          </div>
+          <nav>
+            <ul className={toggle ? "toggle" : ""}>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/products">my_Products</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/add">Create</Link>
+              </li>
+              <li className="close" onClick={this.menuToggle}>
+                <img src={Close} alt="" width="20" />
+              </li>
+            </ul>
+            <div className="nav-user">
+              <Link to="/user/1">
+                <img src={User} alt="user" width="25" />
+              </Link>
+            </div>
+            <div className="nav-cart">
+              <span>0</span>
+              <Link to="/cart">
+                <img src={CartIcon} alt="" width="20" />
+              </Link>
+            </div>
+          </nav>
+        </header>
     );
   }
 }

4be1ca4c61040fe57d511a286e83a45a78eedc83
Azuki04 <syviet@outlook.com>
2023-06-14 18:28:23 +0200

    Bug in User.java fixed

--- Back-End/src/main/java/ch/web/web_shop/model/User.java
+++ Back-End/src/main/java/ch/web/web_shop/model/User.java
@@ -21,7 +21,6 @@ public class User {
 
     }
     public User( String name, String email, boolean subscribed, String password) {
-        this.id = id;
         this.name = name;
         this.email = email;
         this.subscribed = subscribed;
